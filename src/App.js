94beb471b3e3bf51c44c3747ac0f5c47ee970b3b
Prakash-zyzz <prakashvasan08@gmail.com>
2024-06-09 04:27:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { IoCartOutline } from "react-icons/io5";
+import { data } from "./data.js";
+import CrossAir from "./Assets/CORSAIRS.png"
 
 function App() {
+  const [products, setProducts] = useState(data);
+  const [search, setSearch] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-black">
+      <div>
+        <img className="mx-auto py-4" src={CrossAir}/>
+      </div>
+      
+  
+      <div className="w-full h-[80px] bg-black flex justify-center">
+      <Form className="w-[1250px] ">
+        <InputGroup className='my-3 h-12 bg-black'>
+            <Form.Control 
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder="Products"
+                className="bg-gray-200 text-black"
+            />
+        </InputGroup>
+        </Form>
+       </div>
+
+    <div className="bg-black h-full w-full flex justify-center p-4">
+      
+      
+      
+      <div className="flex flex-wrap justify-center max-w-7xl">
+        {data.filter((item)=>{return search.toLowerCase()===""?item:item.title.toLowerCase().includes(search)}).map((item, index) => (
+          <div key={index} className="flex-grow sm:w-1/2 md:w-1/3 p-4">
+            <div className="max-w-sm bg-white shadow dark:bg-gray-800 dark:border-gray-700">
+              <div className="bg-[#151515]">
+                <a>
+                  <img className="m-auto py-4" src={item.image} alt={item.title} />
+                </a>
+              </div>
+              <div className="p-8 bg-[#3f3f3f]">
+                <a >
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white">
+                    {item.title}
+                  </h5>
+                </a>
+                <p className="mb-3 py-4 text-[23px] font-bold text-white dark:text-gray-400">
+                  ${item.price}
+                </p>
+                <p className="mb-3 flex py-2 text-[18px] text-[#ECE81A] hover:text-[#c9c062] cursor-pointer font-bold dark:text-gray-400">
+                  <IoCartOutline size={25} /> <span className="px-2">ADD TO CART</span>
+                </p>
+                
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,61 @@
+export const data = [{
+    "id": 1,
+    "image": "https://assets.corsair.com/image/upload/c_pad,q_auto,h_300,w_300/products/Gaming-Mice/CH-9304211-NA/Gallery/Scimitar_ELITE_BLK_01.webp?w=1920&q=75",
+    "title": "SCIMITAR RGB ELITE Optical MOBA/MMO Gaming Mouse",
+    "price": "59.99USD"
+  }, {
+    "id": 2,
+    "image": "https://assets.corsair.com/image/upload/c_pad,q_auto,h_300,w_300/products/Gaming-Mice/base-m65-rgb-ultra-2-config/Gallery/M65_RGB_ULTRA_01.webp?w=1920&q=75",
+    "title": "M65 RGB ULTRA Tunable FPS Gaming Mouse",
+    "price": "69.99USD"
+  }, {
+    "id": 3,
+    "image": "https://assets.corsair.com/image/upload/c_pad,q_auto,h_300,w_300/products/Gaming-Mice/CH-931D100/M75_AIR_BLACK_RENDER_01.webp?w=1920&q=75",
+    "title": "M75 AIR WIRELESS Ultra-Lightweight Gaming Mouse – Black",
+    "price": "99.99USD"
+  }, {
+    "id": 4,
+    "image": "https://assets.corsair.com/image/upload/c_pad,q_auto,h_300,w_300/products/Gaming-Mousepads/base-mm700-rgb-config/Gallery/MM700_RGB_01.webp?w=1920&q=75",
+    "title": "MM700 RGB Extended Mouse Pad",
+    "price": "49.99USD"
+  }, {
+    "id": 5,
+    "image": "https://assets.corsair.com/image/upload/c_pad,q_auto,h_300,w_300/products/Gaming-Mousepads/base-mm300-pro-config/Gallery/MM300_PRO_Extended_01.webp?w=1920&q=75",
+    "title": "MM300 PRO Premium Spill-Proof Cloth Gaming Mouse Pad — Medium",
+    "price": "22.99USD"
+  }, {
+    "id": 6,
+    "image": "https://assets.corsair.com/image/upload/c_pad,q_auto,h_300,w_300/products/Gaming-Mice/CH-9311011-NA/Gallery/Harpoon_RGB_Wireless_01.webp?w=1920&q=75",
+    "title": "HARPOON RGB WIRELESS Gaming Mouse",
+    "price": "$59.99USD"
+  }, {
+    "id": 7,
+    "image": "https://assets.corsair.com/image/upload/c_pad,q_auto,h_300,w_300/products/Gaming-Mice/CH-9317011-NA/Gallery/IRONCLAW_RGB_WIRELESS_01.webp?w=1920&q=75",
+    "title": "IRONCLAW RGB WIRELESS Gaming Mouse",
+    "price": "79.99USD"
+  }, {
+    "id": 8,
+    "image": "https://assets.corsair.com/image/upload/c_pad,q_auto,h_300,w_300/products/Gaming-Mousepads/CH-9000099-WW/Gallery/MM200_001.webp?w=1920&q=75",
+    "title": "MM200 Cloth Gaming Mouse Pad — Medium",
+    "price": "9.99USD"
+  }, {
+    "id": 9,
+    "image": "https://assets.corsair.com/image/upload/c_pad,q_auto,h_300,w_300/products/Gaming-Mice/CH-9301111-NA/Gallery/Harpoon_RGB_PRO_01.webp?w=1920&q=75",
+    "title": "HARPOON RGB PRO FPS/MOBA Gaming Mouse",
+    "price": "59.99USD"
+  }, {
+    "id": 10,
+    "image": "https://assets.corsair.com/image/upload/c_pad,q_auto,h_300,w_300/products/Gaming-Mice/CH-9306011-NA/Gallery/NIGHTSWORD_RGB_01.webp?w=1920&q=75",
+    "title": "NIGHTSWORD RGB Tunable FPS/MOBA Gaming Mouse",
+    "price": "69.99USD"
+  }, {
+    "id": 11,
+    "image": "https://assets.corsair.com/image/upload/c_pad,q_auto,h_300,w_300/products/Gaming-Mice/CH-9315511-NA/Gallery/DARK_CORE_RGB_PRO_SE_01.webp?w=1920&q=75",
+    "title": "DARK CORE RGB PRO SE Wireless Gaming Mouse",
+    "price": "99.99USD"
+  }, {
+    "id": 12,
+    "image": "https://assets.corsair.com/image/upload/c_pad,q_auto,h_300,w_300/products/Gaming-Mice/base-katar-pro-wireless-config/Gallery/KATAR_PRO_WIRELESS_01.webp?w=1920&q=75",
+    "title": "KATAR PRO Wireless Gaming Mouse",
+    "price": "49.99USD"
+  }]
